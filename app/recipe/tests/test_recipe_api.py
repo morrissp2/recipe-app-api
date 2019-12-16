117b4a0fa6de07ed18584f4cacaa288b62c4acf5
Stephen Morris <stephen-is.morris@mutualofomaha.com>
2019-12-16 12:28:27 -0600

    Added list recipe feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -77,8 +77,6 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data, serializer.data)
-
-    
     # def test_create_ingredient_successful(self):
     #    """Test Creating a new ingredient"""
     #    payload = {'name': 'Test Ingredient'}
@@ -97,4 +95,3 @@ class PrivateRecipeApiTests(TestCase):
     #    res = self.client.post(INGREDIENT_URL, payload)
 
     #   self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
